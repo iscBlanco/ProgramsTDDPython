2aad6f5961301e0108ad146483ba4a8608f1fb7b
unknown <carlosnaalavila@gmail.com>
2015-10-12 15:42:51 -0500

    Segunda mitad del practical_test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+browser.get('http://localhost:8000')
+assert 'TO-DO' in browser.title
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very mehodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes bac to sleep
+
+browser.quit()
