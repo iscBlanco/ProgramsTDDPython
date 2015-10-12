dee1917a39dcab8250abe1820c0ce38ba6fda0e9
iscBlanco <iscblanco@gmail.com>
2015-10-12 16:46:22 -0500

    functional_tests texto agregado

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Edith has heard about a cool new online to-do app. she goes
+# to check out its homepage
 browser.get('http://localhost:8000')
 
+# She notices the page title and header mention to-do lists
 assert 'Django' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
