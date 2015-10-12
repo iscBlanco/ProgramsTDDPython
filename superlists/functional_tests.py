fdc5c2740c25f473da8a43f186c8eb54b27d3fc2
iscBlanco <iscblanco@gmail.com>
2015-10-12 17:20:32 -0500

    capitulo 2do ejemplo

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,35 +1,14 @@
 from selenium import webdriver
+import unittests
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):	#1
 
+    def setUp(self):	#2
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):    #3
+    	self.browser.quit()
 
-# Edith has heard about a cool new online to-do app. she goes
-# to check out its homepage
-browser.get('http://localhost:8000')
-
-# She notices the page title and header mention to-do lists
-assert 'TO-DO' in browser.title
-
-# She is invited to enter a to-do item straight away
-
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
-
-# When she hits enter, the page updates, and now the page lists
-
-# "1: Buy peacock feathers" as an item in a to-do list
-
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very mehodical)
-
-# The page updates again, and now shows both items on her list
-
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
-
-# She visits that URL - her to-do list is still there.
-
-# Satisfied, she goes back to sleep
-
-browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):    #4
+        # Edith has heard about a cool new online to-do app. She goes
+        # to checkout its homepage
