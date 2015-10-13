dc39b6c4844c1f59e75fad4956ea3c40906f5433
iscBlanco <iscblanco@gmail.com>
2015-10-13 14:42:08 -0500

    home_page se agrego en url

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 
 
 urlpatterns = patterns ('',
-    url(r'^$', 'lists.views.home', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
    # url(r'^admin/', include(admin.site.urls)),
 )
