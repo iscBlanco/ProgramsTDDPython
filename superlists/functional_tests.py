2546e69761d3f97debbca28b6f78020d44149b66
carlosadriannaalavila <CarlosNaal@WarCarrier.local>
2015-10-13 16:24:30 -0500

    El programa ahora revisa que introduzcamos un Objeto To-Do

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):	#1
@@ -16,19 +17,31 @@ class NewVisitorTest(unittest.TestCase):	#1
         self.browser.get('http://localhost:8000')
 
         #She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title) #5
-        self.fail('Finish the test!') #6
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
         #She is invited to enter to-do item straight away
+        inputbox = self.browser.finde_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-
+        inputbox.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys('Keys.ENTER')
 
+        table = self.browser.finde_element_by_id('id_list_table')
+        rows = table.finde_element_by_tag_name('tr')
+        self.asserTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
+        self.fail('Finish the test!')
         # The page updates again, and now shows both items on her list
 
         # Edith wonders whether the site will remember her list. Then she sees
@@ -38,6 +51,6 @@ class NewVisitorTest(unittest.TestCase):	#1
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
+        
 if __name__ == '__main__': #7
 	unittest.main(warnings='ignore') #8
