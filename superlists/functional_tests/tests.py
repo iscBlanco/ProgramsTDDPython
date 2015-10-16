200e287b47cbaeb1a303e22bd8cb665b40829e57
iscBlanco <iscblanco@gmail.com>
2015-10-16 15:07:13 -0500

    parte del ejercico del capitulo 6 tests.py functional_tests

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -50,6 +50,19 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith´s list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #Satisfied, they both go back to sleep
+
         
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
