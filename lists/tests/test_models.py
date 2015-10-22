4f8008d7f96434884478440568b158c11a45bcf4
carlosadriannaalavila <carlosnaalavila@gmail.com>
2015-10-21 23:22:04 -0500

    CH10DPPPOSTRSVRF Se ajusta la nueva vista de listas par ahacer la validacion del modelo

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,5 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-
 from lists.models import Item, List
 
 class ListAndItemModelsTest(TestCase):
@@ -7,6 +7,7 @@ class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
         list_ = List()
         list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.list = list_
@@ -29,3 +30,12 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
